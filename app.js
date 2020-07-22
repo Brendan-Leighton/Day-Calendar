16fec4aca8421c33a9253952175b01e35c3d00b1
BR3NDAN <br3ndan.l8n@gmail.com>
2020-07-21 23:27:46 -0400

    Now accepts users input for hour to start work day and sets the hour blocks accordingly

--- app.js
+++ app.js
@@ -3,17 +3,32 @@ $('document').ready(function () {
     
     const userInputStartTime = $("#user-input-start-time");
     const buttonSubmitStartTime = $("#submit-start-time");
-    const setLocalLayout = localStorage.setItem("userScores", JSON.stringify(savedScheduleLayout));
-    const setLocalTextData = localStorage.setItem("userScores", JSON.stringify(savedTextareaData));
+    
     function displayTimeSlots() {
         console.log(`function run`);
-        for (i = 0; i < 9; i++) {
+        for (let i = 0; i < 9; i++) {
             console.log(`for loop accessed`);
-            const timeDiv = $("<div>").addClass(`col-4`).attr("id", `time-display-${i}`);
+            let hour = parseInt(userInputStartTime.val()) + i;
+            if (hour > 12) {
+                hour -= 12;
+            }
+            timeSignature = `${hour}:00`;
+            console.log(hour);
+            console.log(timeSignature);
+            const timeDiv = $("<div>").addClass(`col-4`).attr("id", `time-display-${i}`).text(`${hour}:00`);
             const textArea = $("<textarea>").addClass(`col-8`).attr("id", `textarea-${i}`);
             timeSlotSection.append(timeDiv);
             timeSlotSection.append(textArea);
         }
+    }
+
+    function saveToLocal(layout_or_textareaData) {
+        if (this === 'layout') {
+            JSON.parse(localStorage.getItem("userScores"));
+            localStorage.setItem("userScores", JSON.stringify(savedScheduleLayout));
+        }
+        const setLocalLayout = localStorage.setItem("userScores", JSON.stringify(savedScheduleLayout));
+        const setLocalTextData = localStorage.setItem("userScores", JSON.stringify(savedTextareaData));
     }
 
     buttonSubmitStartTime.on('click', function () {
